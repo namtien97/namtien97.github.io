72ed547f26d6d7257d7087f2e031266ab0e3af33
{NamTienNguyen} <{namtien97@gmail,com}>
2020-10-14 22:43:40 +0700

    StudentDatabaseApp

---
+++ JavaMiniProject/src/StudentDatabaseApp/StudentDatabaseApp.java
@@ -0,0 +1,24 @@
+package StudentDatabaseApp;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        System.out.print("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+
+        for (int n = 0; n < numOfStudents; n++) {
+            System.out.println("----------------------");
+            System.out.println(students[n].toString());
+        }
+        System.out.println("----------------------");
+    }
+}
